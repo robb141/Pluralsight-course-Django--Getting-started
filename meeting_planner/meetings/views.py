43d838496b5918b7f1d84be061e659dd664473c7
Robert Solava <Robert.solava@ing.com>
2022-03-29 17:32:10 +0200

    Finished simple website with learning course.

---
+++ meeting_planner/meetings/views.py
@@ -0,0 +1,46 @@
+from django.shortcuts import render, get_object_or_404, redirect
+
+from .models import Meeting, Room
+from .forms import MeetingForm
+
+
+def detail(request, id):
+    meeting = get_object_or_404(Meeting, pk=id)
+    return render(request, "meetings/detail.html", {"meeting": meeting})
+
+
+def rooms_list(request):
+    return render(request, "meetings/rooms_list.html",
+                  {"rooms": Room.objects.all()})
+
+
+def new(request):
+    if request.method == "POST":
+        form = MeetingForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("welcome")
+    else:
+        form = MeetingForm()
+    return render(request, "meetings/new.html", {"form": form})
+
+
+def edit(request, id):
+    meeting = get_object_or_404(Meeting, pk=id)
+    if request.method == "POST":
+        form = MeetingForm(request.POST, instance=meeting)
+        if form.is_valid():
+            form.save()
+            return redirect("detail", id)
+    else:
+        form = MeetingForm(instance=meeting)
+    return render(request, "meetings/edit.html", {"form": form})
+
+
+def delete(request, id):
+    meeting = get_object_or_404(Meeting, pk=id)
+    if request.method == "POST":
+        meeting.delete()
+        return redirect("welcome")
+    else:
+        return render(request, "meetings/confirm_delete.html", {"meeting": meeting})
---
+++ meeting_planner/website/views.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+
+from django.shortcuts import render
+from django.http import HttpResponse
+
+from meetings.models import Meeting
+
+
+def welcome(request):
+    return render(request, "website/welcome.html",
+                  {"meetings": Meeting.objects.all()})
+
+
+def date(request):
+    return HttpResponse("This page was served at " + str(datetime.now()))
+
+
+def about(request):
+    return HttpResponse("Hi, my name is Robert and I like programming with Django.")
+
+
+
